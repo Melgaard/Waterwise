1159c2ca2501adf36c931016b784f29e2ff49d72
Jernsax <emn.jernsax@gmail.com>
2015-05-25 11:21:51 +0200

    stacktrace

--- Waterwise/src/waterwise/ElementListCollection.java
+++ Waterwise/src/waterwise/ElementListCollection.java
@@ -13,16 +13,16 @@ public class ElementListCollection {
     private static ArrayList<Order> oList = new ArrayList<>();
     private static ArrayList<Customer> cList = new ArrayList<>();
     private static ArrayList<Order> stockList = new ArrayList<>();
-    
+
     //Setter
     public static void addPList(Product pToAdd) {
         pList.add(pToAdd);
     }
-    
+
     public static void addOrder(Order newOrder) {
         oList.add(newOrder);
     }
-    
+
     public static void addCList(Customer cToAdd) {
         cList.add(cToAdd);
     }
@@ -37,39 +37,43 @@ public class ElementListCollection {
         try {
             pList = fw.loadProductList();
         } catch (Exception ex) {
-            System.out.println( ex + " thrown from - ElementListCollection");
+            System.out.println(ex + " thrown from - ElementListCollection");
+            ex.printStackTrace();
         }
         return pList;
     }
-    
+
     public static ArrayList<Order> getOList() {
         FileWrapper fw = new FileWrapper();
         try {
             oList = fw.loadIncomingOrderList();
         } catch (Exception ex) {
-            System.out.println( ex + " thrown from - ElementListCollection");
+            System.out.println(ex + " thrown from - ElementListCollection");
+            ex.printStackTrace();
         }
         return oList;
     }
-    
+
     public static ArrayList<Customer> getCList() {
         FileWrapper fw = new FileWrapper();
         try {
             cList = fw.loadCustomerList();
         } catch (Exception ex) {
-            System.out.println( ex + " thrown from - ElementListCollection");
+            System.out.println(ex + " thrown from - ElementListCollection");
+            ex.printStackTrace();
         }
         return cList;
     }
-    
+
     public static ArrayList<Order> getStockList() {
         FileWrapper fw = new FileWrapper();
         try {
             stockList = fw.loadOutgoingOrderList();
         } catch (Exception ex) {
-            System.out.println( ex + " thrown from - ElementListCollection");
+            System.out.println(ex + " thrown from - ElementListCollection");
+            ex.printStackTrace();
         }
         return stockList;
     }
-    
+
 }
